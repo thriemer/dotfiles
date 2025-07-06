9d9239df7dea603c9638b4dc0ca5f3eb822c0aed
exlthriemer <linus.thriemer@pentacor.de>
2025-07-06 23:43:21 +0200

    38 current  2025-07-06 23:43:17  25.05.805852.29e290002bff  6.12.35                          *

--- NixOs/configuration.nix
+++ NixOs/configuration.nix
@@ -21,6 +21,7 @@
     wireless.iwd.enable = true;
     networkmanager.wifi.backend = "iwd";
     wireguard.enable = true;
+    firewall.allowedUDPPorts = [51820];
   };
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
@@ -223,12 +224,6 @@
     };
     ssh.startAgent = true; #remeber private keys so that i dont have to type them in again
   };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
